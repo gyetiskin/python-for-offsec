ada7f8aaf7b3d92967ce1b272a8e07735c0f4040
Gurur Yetiskin <59870590+gyetiskin@users.noreply.github.com>
2022-07-10 15:54:04 +0300

    Create tcp_attacker.py

---
+++ tcp_attacker.py
@@ -0,0 +1,25 @@
+import socket
+
+host = "localhost"
+port = 2468
+
+try:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    s.bind((host, port)) 
+    print("socket {} connected to the port".format(port))
+
+    s.listen(1)      
+    print("listening")
+except socket.error as msg:
+    print("error:",msg)
+
+while True: 
+
+   c, addr = s.accept()      
+   print('connection', addr)
+
+   mesaj = 'connected'
+   c.send(mesaj.encode('utf-8')) 
+
+   c.close()
